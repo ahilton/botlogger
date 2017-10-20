fa055398aa03794cfcca5f84a23b727411130311
zut <ahilton@gmail.com>
2017-10-20 23:28:34 +1100

    capture user and timestamp

--- src/main/java/org/bttf/botlogger/model/OrderLogEntry.java
+++ src/main/java/org/bttf/botlogger/model/OrderLogEntry.java
@@ -7,16 +7,20 @@ public class OrderLogEntry {
 
     /* Example raw JSON:
 
-            { conversationId: 'm4lg0ia6a43',
-          channel: 'emulator',
-          lastUserMessage: 'microsoft',
-          lastOrderState:
-           { stock: 'Microsoft',
-             qty: 50000,
-             direction: 'Sell',
-             completed: true },
-          lastSystemMessage: 'How many Microsoft would you like to order?',
-          choices: [] }
+           {conversationId: 'm4lg0ia6a43',
+            channel: 'emulator',
+            lastUserMessage: 'microsoft',
+            lastOrderState:
+            {   stock: 'Microsoft',
+                qty: 50000,
+                direction: 'Sell',
+                completed: true
+            },
+            lastSystemMessage: 'How many Microsoft would you like to order?',
+            choices: [],
+            userId: 'default-user',
+            userName: 'User',
+            timestamp: '2017-10-20T12:11:04.744Z' }
 
      */
 
@@ -26,5 +30,8 @@ public class OrderLogEntry {
     public OrderState lastOrderState;
     public String lastSystemMessage;
     public String[] choices;
+    public String userId;
+    public String userName;
+    public String timestamp;
 
 }
